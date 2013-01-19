490ba753402e3611971a2601227eb54084ab581f
Evgenia Manolova <evgenia@empowerunited.com>
2013-01-19 18:47:09 +0200

    Created application using Refinery CMS 2.0.9

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SolELua::Application.config.secret_token = '7da9af00062f5b563e80989593c4140f8a093c5106191cb19c085e904e33ae68d3f3ac198413296ff57a28d000471511517eada56a2cd9ed8a3e0f15632c4ebd'
